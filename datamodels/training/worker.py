2feeb26b51f4703260c78afdcd9fd5d35fbf1db7
Paul Lintilhac <paullintilhac@vpn-investment-office-231-128-148.dartmouth.edu>
2024-07-05 00:32:29 -0400

    asdf

--- datamodels/training/worker.py
+++ datamodels/training/worker.py
@@ -50,6 +50,7 @@ def kv_log(k, v, logdir, index):
     # now acutally open it for writing
     mmap = get_mmap(this_filename, 'r+')
     print("v: " + str(v) + ", index: " +str(index))
+    print("len(v): " + str(len(v)) + ", len(mmap): " + str(len(mmap)))
     mmap[index] = v
     mmap.flush()
 
