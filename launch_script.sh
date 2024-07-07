e4c3b71de12620325532673c0fc35c561f68cfa4
Paul S. Lintilhac <d28979q@slurm-fe01-prd.dartmouth.edu>
2024-07-07 14:24:10 -0400

    rebase with soft reset

---
+++ launch_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --account=temfom0  # Specify the account to charge
+
+#SBATCH --job-name=my_job  # Job name
+
+#SBATCH --output=jobs/my_job_%j.out  # Standard output and error log
+
+#SBATCH --error=jobs/my_job_%j.err  
+#SBATCH --time=48:00:00  # Time limit hrs:min:sec
+
+#SBATCH --partition=gpuq  # Specify the partition to submit to
+#SBATCH --gres=gpu:1
+echo $(hostname -s)
+
+source activate ffcv2
+nvcc --version
+echo conda env:
+
+./examples/cifar10/example.sh
