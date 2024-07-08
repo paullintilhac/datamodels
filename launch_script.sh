664dd392bc7002fca1c67876f35939a39ac1e4cc
Paul S. Lintilhac <d28979q@slurm-fe01-prd.dartmouth.edu>
2024-07-08 16:42:36 -0400

    commit before merge

--- launch_script.sh
+++ launch_script.sh
@@ -7,10 +7,10 @@
 #SBATCH --output=jobs/my_job_%j.out  # Standard output and error log
 
 #SBATCH --error=jobs/my_job_%j.err  
-#SBATCH --time=48:00:00  # Time limit hrs:min:sec
+#SBATCH --time=80:00:00  # Time limit hrs:min:sec
 
 #SBATCH --partition=gpuq  # Specify the partition to submit to
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 echo $(hostname -s)
 
 source activate ffcv2
