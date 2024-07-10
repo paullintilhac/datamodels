aca5b44afc243ae4f7a653270216420e9a340f77
Paul Lintilhac <paullintilhac@vpn-investment-office-231-128-169.dartmouth.edu>
2024-07-09 22:00:02 -0400

    updated file paths for colab

--- download_cifar.py
+++ download_cifar.py
@@ -12,12 +12,12 @@ from ffcv.transforms import RandomHorizontalFlip, Cutout, \
 from ffcv.transforms.common import Squeeze
 from ffcv.writer import DatasetWriter
 datasets = {
-    'train': torchvision.datasets.CIFAR10('/dartfs/rc/lab/C/CybenkoG/cifar-ffcv', train=True, download=True),
-    'test': torchvision.datasets.CIFAR10('/dartfs/rc/lab/C/CybenkoG/cifar-ffcv', train=False, download=True)
+    'train': torchvision.datasets.CIFAR10('/content', train=True, download=True),
+    'test': torchvision.datasets.CIFAR10('/content', train=False, download=True)
 }
 
 for (name, ds) in datasets.items():
-    writer = DatasetWriter(f'/dartfs/rc/lab/C/CybenkoG/cifar-ffcv/cifar_{name}.beton', {
+    writer = DatasetWriter(f'/content/cifar_{name}.beton', {
         'image': RGBImageField(),
         'label': IntField()
     })
