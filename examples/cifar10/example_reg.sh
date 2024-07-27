aec53138b36dc46ac7f6e232e67922e4c4f66a16
Paul Lintilhac <paullintilhac@vpn-investment-office-231-128-245.dartmouth.edu>
2024-07-27 17:58:20 -0400

    change to confidences

--- examples/cifar10/example_reg.sh
+++ examples/cifar10/example_reg.sh
@@ -9,7 +9,7 @@ tmp_dir=/content/drive/Shareddrives/TEMFOM/tmp/combined_onion
 python -m datamodels.regression.write_dataset \
              --cfg.data_dir $tmp_dir \
              --cfg.out_path "$tmp_dir/reg_data.beton" \
-             --cfg.y_name margins \
+             --cfg.y_name confidences \
              --cfg.x_name masks
 echo "> regression data prepared!"
 
@@ -19,4 +19,4 @@ python -m datamodels.regression.compute_datamodels \
     --data.data_path "$tmp_dir/reg_data.beton" \
     --cfg.out_dir "$tmp_dir/reg_results_onion"
 echo "> regression DONE!"
-echo "> Datamodels stored in: $tmp_dir/reg_results_onion/datamodels.pt" 
+echo "> Datamodels stored in: $tmp_dir/reg_results_onion/datamodels_confidences.pt" 
