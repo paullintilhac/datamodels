a1ad05e1dca226a4ac1fb4187c02052e4e300728
Paul Lintilhac <paullintilhac@vpn-two-factor-general-231-128-218.dartmouth.edu>
2024-08-08 19:11:25 -0400

    attempted fix

--- examples/cifar10/train_cifar.py
+++ examples/cifar10/train_cifar.py
@@ -185,7 +185,7 @@ def evaluate(model, loaders, lr_tta=False):
                 probs[:,labs]=0
                 wrong_probs = ch.sum(probs,1)
                 final_score = ch.log(correct_probs+1e-45)-ch.log(wrong_probs+1e-45)
-                print("final score shape: " + str(final_score.shape))
+                print("final score shape: " + str(final_score.shape) + ", class logits shape: " + str(class_logits.shape))
                 all_confidences.append(final_score.cpu())
                 class_logits = class_logits.clone()
                 
